df4b79f3b3435b382f95d89f69a3858da2e53162
jang <97jjy97@naver.com>
2022-07-29 14:45:02 +0900

    css캐싱

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,8 +4,8 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>신승지류유통</title>
-        <link rel="stylesheet" href="{{ URL::asset('css/normalize.css') }}">
-        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('css/normalize.css') }}?12">
+        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}?12">
         <script src="{{ URL::asset('js/jquery-3.6.0.min.js') }}"></script>
     </head>
     <body>
