6a363ff1ff99ddb5d38ceda4bf0e9e6f84a7e2dd
jang <97jjy97@naver.com>
2022-08-02 17:58:46 +0900

    스타일 캐시 제거

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,8 +4,8 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>신승지류유통</title>
-        <link rel="stylesheet" href="{{ URL::asset('css/normalize.css') }}?12">
-        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}?12">
+        <link rel="stylesheet" href="{{ URL::asset('css/normalize.css') }}?<?php echo time()?>">
+        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}?<?php echo time()?>">
         <script src="{{ URL::asset('js/jquery-3.6.0.min.js') }}"></script>
     </head>
     <body>
