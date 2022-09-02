a7b4791e9346fbe9fe4fd24b3ee9ccbb2e5aeba4
jang <97jjy97@naver.com>
2022-09-02 17:20:11 +0900

    테스트

--- resources/views/employment/recruit.blade.php
+++ resources/views/employment/recruit.blade.php
@@ -152,7 +152,7 @@
                     <span class="text">민감정보 수집 및 이용에 동의합니다.</span>
                 </div>
             </div>
-            <!-- <div class="inquire-input">
+            <div class="inquire-input">
                 <div class="inquire full">
                     <p class="title need">지원분야<span>*</span></p>
                     <div class="input-layout">
@@ -199,11 +199,11 @@
                         <input type="text" placeholder="5MB이하 파일만 첨부 가능합니다.">
                     </div>
                 </div>
-                <div class="inquire full">
+                <!-- <div class="inquire full">
                     <p class="title need">자기소개서<span>*</span></p>
                     <textarea name="" id=""></textarea>
-                </div>
-            </div> -->
+                </div> -->
+            </div>
             <div class="btn-area area-center">
                 <a href="javascript:;" class="btn btn-white">취소하기</a>
                 <a href="javascript:;" class="btn btn-blue">지원하기</a>
