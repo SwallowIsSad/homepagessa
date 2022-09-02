d90bc37942a8429182ae36c55c0a85f5b6027263
jang <97jjy97@naver.com>
2022-09-02 17:12:02 +0900

    페에지 흔들림 수정

--- resources/views/employment/recruit.blade.php
+++ resources/views/employment/recruit.blade.php
@@ -83,73 +83,73 @@
     <div class="inquire-form">
         <form action="#">
             <div class="personal-info m50b">
-                <p class="info">개인정보 수집·활용 동의서<span>*</span></p>
-                <div class="pi-text">
-                    개인정보 보호를 위한 이용자 동의사항.
-                    <br>
-                    <br>
-                    신승지류유통은 개인정보보호법 등 관련 법률에 따라 개인정보 수집 시 정보 주체에게 사전 고지하고 이에 대한 동의를 받습니다.
-                    <br>
-                    - 개인정보의 수집 및 이용 목적 : 신승지류유통 고객센터 문의하기 서비스 이용
-                    <br>
-                    - 수집하는 개인정보 항목 : 업종, 업체명, 이름, 주소, 연락처, 이메일주소
-                    <br>
-                    - 개인정보 보유 및 이용 기간 : 동의일로부터 5년
-                    <br>
-                    <br>
-                    귀하는 동의를 거부할 권리가 있으나, 상기 사항에 동의하지 않으실 경우 문의하기 이용에 제한을 받으실 수 있습니다.
+                <p class="info">개인정보 수집·활용 동의서</p>
+                <div class="pi-content">
+                    <div class="pi-title">
+                        약관 펼쳐보기
+                        <img src="{{ URL::asset('img/cs/personal-arrow.png') }}" alt="">
+                    </div>
+                    <div class="pi-text" style="display:none">
+                        개인정보 보호를 위한 이용자 동의사항.
+                        <br>
+                        <br>
+                        신승지류유통은 개인정보보호법 등 관련 법률에 따라 개인정보 수집 시 정보 주체에게 사전 고지하고 이에 대한 동의를 받습니다.
+                        <br>
+                        - 개인정보의 수집 및 이용 목적 : 신승지류유통 고객센터 문의하기 서비스 이용
+                        <br>
+                        - 수집하는 개인정보 항목 : 업종, 업체명, 이름, 주소, 연락처, 이메일주소
+                        <br>
+                        - 개인정보 보유 및 이용 기간 : 동의일로부터 5년
+                        <br>
+                        <br>
+                        귀하는 동의를 거부할 권리가 있으나, 상기 사항에 동의하지 않으실 경우 문의하기 이용에 제한을 받으실 수 있습니다.
+                    </div>
                 </div>
                 <div class="check-text">
-                    <p style="margin-top:20px;">
-                    <span class="check"><img src="{{ URL::asset('img/cs/check.png') }}" alt=""></span>
-                    개인정보 수집·활용에 동의합니다.
-                </p>
+                    <span class="check">
+                        <div class="on" style="display:none">
+                            <img src="{{ URL::asset('img/cs/check-on.png') }}" alt="">
+                        </div>
+                        <div class="off">
+                            <img src="{{ URL::asset('img/cs/check-off.png') }}" alt="">
+                        </div>
+                    </span>
+                    <span class="text">개인정보 수집·활용에 동의합니다.</span>
                 </div>
             </div>
             <div class="personal-info">
-                <p class="info">민감정보 수집 및 이용 동의<span>*</span></p>
-                <div class="pi-text">
-                    신승지류유통㈜ (이하 ‘회사’)은 아래와 같이 민감정보를 수집 및 이용합니다.
-                    <br>
-                    <br>
-                    1. 수집 · 이용 항목
-                    <br>
-                    <br>
-                    공고 지원 시 다음 민감정보를 수집 및 이용합니다.
-                    <br>
-                    <br>
-                    - 보훈사항 : 보훈여부 (대상자일 경우-보훈번호, 보훈구분, 관계)
-                    <br>
-                    - 장애사항 : 장애여부 (대상자일 경우-장애내용, 장애정도)
-                    <br>
-                    2. 수집 · 이용 목적
-                    <br>
-                    <br>
-                    수집한 개인정보는 아래 목적으로 이용됩니다.
-                    <br>
-                    <br>
-                    - 국가유공자, 장애인 등 취업보호대상자 우대
-                    <br>
-                    3. 보유 및 이용기간
-                    <br>
-                    <br>
-                    채용 여부 확정일로부터 180일 보관 후 파기합니다.
-                    <br>
-                    <br>
-                    - 지원자 계정 삭제 및 지원서 삭제 시 즉시 파기합니다.
-                    <br>
-                    (단, 상시 채용을 위한 정보 수집 및 이용에 동의하는 경우 지원 전형 종료 이후 최대 5년까지 보관)
-                    <br>
-                    민감정보 수집‧이용 동의를 거부할 권리가 있습니다.
-                    <br>
-                    그러나 동의를 거부할 경우 원활한 심사를 할 수 없어 영입에 제한을 받을 수 있습니다.
-                    <br>
+                <p class="info">민감정보 수집 및 이용 동의</p>
+                <div class="pi-content">
+                    <div class="pi-title">
+                        약관 펼쳐보기
+                        <img src="{{ URL::asset('img/cs/personal-arrow.png') }}" alt="">
+                    </div>
+                    <div class="pi-text" style="display:none">
+                        개인정보 보호를 위한 이용자 동의사항.
+                        <br>
+                        <br>
+                        신승지류유통은 개인정보보호법 등 관련 법률에 따라 개인정보 수집 시 정보 주체에게 사전 고지하고 이에 대한 동의를 받습니다.
+                        <br>
+                        - 개인정보의 수집 및 이용 목적 : 신승지류유통 고객센터 문의하기 서비스 이용
+                        <br>
+                        - 수집하는 개인정보 항목 : 업종, 업체명, 이름, 주소, 연락처, 이메일주소
+                        <br>
+                        - 개인정보 보유 및 이용 기간 : 동의일로부터 5년
+                        <br>
+                        <br>
+                        귀하는 동의를 거부할 권리가 있으나, 상기 사항에 동의하지 않으실 경우 문의하기 이용에 제한을 받으실 수 있습니다.
+                    </div>
                 </div>
                 <div class="check-text">
-                    <p style="margin-top:20px;">
-                    <span class="check"><img src="{{ URL::asset('img/cs/check.png') }}" alt=""></span>
-                    개인정보 수집·활용에 동의합니다.
-                </p>
+                    <span class="check">
+                        <div class="on" style="display:none">
+                            <img src="{{ URL::asset('img/cs/check-on.png') }}" alt="">
+                        </div>
+                        <div class="off">
+                            <img src="{{ URL::asset('img/cs/check-off.png') }}" alt="">
+                        </div>
+                    </span>
+                    <span class="text">민감정보 수집 및 이용에 동의합니다.</span>
                 </div>
             </div>
             <div class="inquire-input">
