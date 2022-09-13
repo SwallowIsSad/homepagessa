1e855b614b5f0a33fea392d20e05ce735aedb55e
jang <97jjy97@naver.com>
2022-09-13 15:47:46 +0900

    오시는길

--- resources/views/cs/inquire.blade.php
+++ resources/views/cs/inquire.blade.php
@@ -105,7 +105,7 @@
         <div class="inquire-form">
             <img src="{{ URL::asset('img/cs/map1.png') }}" alt="" style="width:100%">
             <p class="zooso m10b">서울 영업센터 : 서울특별시 중구 마른내로116 신승빌딩 / 02 - 2270 - 4900</p>
-            <img src="{{ URL::asset('img/cs/map2.png') }}" alt="" style="width:100%; margin-top:20px;">
+            <img src="{{ URL::asset('img/cs/map2.png') }}" alt="" style="width:100%; margin-top:30px;">
             <p class="zooso m10b">파주 영업센터 : 경기도 파주시 산업단지길179 2층 / 02 - 2270 - 4900</p>
         </div>
         <?php else:?>
