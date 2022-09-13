5daf928024b15d40e44db80fda90cb84f42bfed7
jang <97jjy97@naver.com>
2022-09-13 10:50:34 +0900

    홍보센터 모바일 제거

--- resources/views/info/publicity.blade.php
+++ resources/views/info/publicity.blade.php
@@ -13,7 +13,9 @@
         <span class="icon">···</span>
     </div>
 </div>
-@include('layout.infolnb')
+<?php if(!preg_match('/(alcatel|amoi|android|avantgo|blackberry|benq|cell|cricket|docomo|elaine|htc|iemobile|iphone|ipad|ipaq|ipod|j2me|java|midp|mini|mmp|mobi|motorola|nec-|nokia|palm|panasonic|philips|phone|sagem|sharp|sie-|smartphone|sony|symbian|t-mobile|telus|up\.browser|up\.link|vodafone|wap|webos|wireless|xda|xoom|zte)/i', $_SERVER['HTTP_USER_AGENT'])):?>
+    @include('layout.infolnb')
+<?php endif;?>
 <div class="sub-page-layout" id="#info-content3">
     <div class="company-layout">
         <img src="{{ URL::asset('img/banner/infotest2.png') }}" alt="" style="max-width:100%">
