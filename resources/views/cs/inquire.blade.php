f9a684ab80e259fc2557b859d7d3d899000ffb4a
jang <97jjy97@naver.com>
2022-09-13 15:46:08 +0900

    오시는길

--- resources/views/cs/inquire.blade.php
+++ resources/views/cs/inquire.blade.php
@@ -104,11 +104,9 @@
         <?php if(!preg_match('/(alcatel|amoi|android|avantgo|blackberry|benq|cell|cricket|docomo|elaine|htc|iemobile|iphone|ipad|ipaq|ipod|j2me|java|midp|mini|mmp|mobi|motorola|nec-|nokia|palm|panasonic|philips|phone|sagem|sharp|sie-|smartphone|sony|symbian|t-mobile|telus|up\.browser|up\.link|vodafone|wap|webos|wireless|xda|xoom|zte)/i', $_SERVER['HTTP_USER_AGENT'])):?>
         <div class="inquire-form">
             <img src="{{ URL::asset('img/cs/map1.png') }}" alt="" style="width:100%">
-            <p class="zooso">서울 본사 / 서울 영업센터 : 서울특별시 중구 마른내로116 신승빌딩, 02 - 2270 - 4900</p>
-            <div class="hr"></div>
+            <p class="zooso m10b">서울 영업센터 : 서울특별시 중구 마른내로116 신승빌딩 / 02 - 2270 - 4900</p>
             <img src="{{ URL::asset('img/cs/map2.png') }}" alt="" style="width:100%">
-            <p class="zooso">파주 영업센터 : 경기도 파주시 산업단지길179 2층, 02 - 2270 - 4900</p>
-            <div class="hr"></div>
+            <p class="zooso m10b">파주 영업센터 : 경기도 파주시 산업단지길179 2층 / 02 - 2270 - 4900</p>
         </div>
         <?php else:?>
         <div class="inquire-form">
