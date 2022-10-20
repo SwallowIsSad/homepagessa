ca5ec4fc6e2062cf7587cc920c15de819e7c1538
name_jay <97jjy97@naver.com>
2022-10-20 12:54:08 +0900

    관리자 중간 백업

--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\BoardService;
 
 
 class NoticeController extends Controller
 {
+
+
     public $menu = ['공지사항', '정보공시', '주문시스템', '홍보센터'];
 
     public function notice()
--- resources/views/admin/notice/list.blade.php
+++ resources/views/admin/notice/list.blade.php
@@ -31,7 +31,10 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">DataTable with minimal features &amp; hover style</h3>
+                            <h3 class="card-title">
+                                <?php var_dump($boardList);?>
+                                DataTable with minimal features &amp; hover style
+                            </h3>
                         </div>
                         <div class="card-body">
                             <div id="example2_wrapper" class="dataTables_wrapper dt-bootstrap4">
@@ -52,76 +55,12 @@
                                                 </tr>
                                             </thead>
                                             <tbody>
+                                                <?php foreach($boardList as $key => $value):?>
                                                 <tr class="odd">
                                                     <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
-                                                    <td>Firefox 1.0</td>
-                                                    <td>Win 98+ / OSX.2+</td>
-                                                    <td>1.7</td>
-                                                    <td>A</td>
+                                                    <td><?php var_dump($value->hit);?></td>
                                                 </tr>
-                                                <tr class="even">
-                                                    <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
-                                                    <td>Firefox 1.5</td>
-                                                    <td>Win 98+ / OSX.2+</td>
-                                                    <td>1.8</td>
-                                                    <td>A</td>
-                                                </tr>
-                                                <tr class="odd">
-                                                    <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
-                                                    <td>Firefox 2.0</td>
-                                                    <td>Win 98+ / OSX.2+</td>
-                                                    <td>1.8</td>
-                                                    <td>A</td>
-                                                </tr>
-                                                <tr class="even">
-                                                    <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
-                                                    <td>Firefox 3.0</td>
-                                                    <td>Win 2k+ / OSX.3+</td>
-                                                    <td>1.9</td>
-                                                    <td>A</td>
-                                                </tr>
-                                                <tr class="odd">
-                                                    <td class="sorting_1 dtr-control">Gecko</td>
-                                                    <td>Camino 1.0</td>
-                                                    <td>OSX.2+</td>
-                                                    <td>1.8</td>
-                                                    <td>A</td>
-                                                </tr>
-                                                <tr class="even">
-                                                    <td class="sorting_1 dtr-control">Gecko</td>
-                                                    <td>Camino 1.5</td>
-                                                    <td>OSX.3+</td>
-                                                    <td>1.8</td>
-                                                    <td>A</td>
-                                                </tr>
-                                                <tr class="odd">
-                                                    <td class="sorting_1 dtr-control">Gecko</td>
-                                                    <td>Netscape 7.2</td>
-                                                    <td>Win 95+ / Mac OS 8.6-9.2</td>
-                                                    <td>1.7</td>
-                                                    <td>A</td>
-                                                </tr>
-                                                <tr class="even">
-                                                    <td class="sorting_1 dtr-control">Gecko</td>
-                                                    <td>Netscape Browser 8</td>
-                                                    <td>Win 98SE+</td>
-                                                    <td>1.7</td>
-                                                    <td>A</td>
-                                                </tr>
-                                                <tr class="odd">
-                                                    <td class="sorting_1 dtr-control">Gecko</td>
-                                                    <td>Netscape Navigator 9</td>
-                                                    <td>Win 98+ / OSX.2+</td>
-                                                    <td>1.8</td>
-                                                    <td>A</td>
-                                                </tr>
-                                                <tr class="even">
-                                                    <td class="sorting_1 dtr-control">Gecko</td>
-                                                    <td>Mozilla 1.0</td>
-                                                    <td>Win 95+ / OSX.1+</td>
-                                                    <td>1</td>
-                                                    <td>A</td>
-                                                </tr>
+                                                <?php endforeach;?>
                                             </tbody>
                                             <tfoot>
                                                 <tr>
