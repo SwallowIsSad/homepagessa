c9953bd5e271f5f10ed98e1088d0b99b99d1fe17
name_jay <97jjy97@naver.com>
2022-11-15 16:05:28 +0900

    오타 수정

--- app/Http/Controllers/Admin/AdminNoticeController.php
+++ app/Http/Controllers/Admin/AdminNoticeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\controllers\Controller;
+use App\Http\Controllers\Controller;
 use App\Http\Requests\AdminNoticeDetailRequest;
 use App\Http\Requests\AdminNoticeAddRequest;
 use App\Http\Requests\AdminNoticeEditRequest;
