4082ffaab4293ed05d09a3b5102e52080bbcc82d
12 <97jjy97@naver.com>
2025-06-24 15:43:32 +0900

    오타수정

--- resources/views/business/cooperative.blade.php
+++ resources/views/business/cooperative.blade.php
@@ -3,7 +3,7 @@
 <div class="company-layout">
     <div class="cooperative">
         <p class="title m10b">(주)신승아이엔씨 협력업체</p>
-        <p class="desc">(주)신승아이엔씨는는 각 전문 분야 전문 업체들과 협력중입니다.</p>
+        <p class="desc">(주)신승아이엔씨는 각 전문 분야 전문 업체들과 협력중입니다.</p>
         <img src="{{ URL::asset('img/company/cooperative.png') }}" alt="">
     </div>
 </div>
